9123b850ee19744228b0cac7d1be068f7c4b918c
Nikita Zotov <72747655+zotOFF-dev@users.noreply.github.com>
2022-02-18 19:15:04 +0700

    Add files via upload

---
+++ Депозитный калькулятор/Депозитный калькулятор/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Депозитный_калькулятор
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Депозитный калькулятор");
+            double P=0, S=0, I=0, t=0;
+            const int K = 365;
+            Console.WriteLine("Введите сумму вклада");
+            P= Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите процентную ставку");
+            I = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите количество дней  на которое будет рассчитан вклад");
+            t = Convert.ToDouble(Console.ReadLine());
+            S = P + (P * I * t) / (K * 100);
+            Console.WriteLine("Конечная сумма вклада= " + S);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ площадь прямоугольника/площадь прямоугольника/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("площадь прямоугольника")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("площадь прямоугольника")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("2c3a425d-242f-4fff-bb5e-af5d6b2ef95a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
