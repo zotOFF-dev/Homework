7328bab67e656e7957b21474ea4a3204acc3e9f2
Nikita Zotov <72747655+zotOFF-dev@users.noreply.github.com>
2022-04-02 23:03:52 +0700

    Delete Program.cs

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.WriteLine("Числа от 35 до 87");
-for (int i=35; i<=85; i++)
-{
-    int k = i % 7;
-    if (k == 1 || k == 2 || k == 5)
-        Console.WriteLine($"Числа кратные 7 и имеющие остаток 1,2 или 5 = {i}");
-}
-Console.ReadKey();  
